f430cbbd930a5c631d32bf38ffbe8e8f63aae80d
Isaac Elias <nicecurelom@gmail.com>
2013-07-26 15:23:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datyr::Application.config.secret_token = 'c70af0bb8bf7c21320347b520792b9a27fec9d79a35364a8bbfc8d1ffc06a261899ea6472ae129659e58f7439c68b0226858140476e5edf8c49ee7fd274f0405'
